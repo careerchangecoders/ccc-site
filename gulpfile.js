e752d64220f66fe21ca060c3789e81f3c3461697
David Lower <davidlower4@gmail.com>
2018-11-11 22:02:36 +0000

    Fix buttons clashing together in Hero. Removed italics from the site (wrong use of italics for UI). Slightly reduced font-size of buttons in hero. Added my social details.

--- gulpfile.js
+++ gulpfile.js
@@ -10,73 +10,73 @@ var browserSync = require('browser-sync').create();
 
 // Set the banner content
 var banner = ['/*!\n',
-  ' * Career Change Coders\n',
-  ' * Copyright ' + (new Date()).getFullYear(),'\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/careerchangecoders/ccc-site/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
+   ' * Career Change Coders\n',
+   ' * Copyright ' + (new Date()).getFullYear(), '\n',
+   ' * Licensed under <%= pkg.license %> (https://github.com/careerchangecoders/ccc-site/blob/master/LICENSE)\n',
+   ' */\n',
+   '\n'
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function() {
 
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+   // Bootstrap
+   gulp.src([
+         './node_modules/bootstrap/dist/**/*',
+         '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+         '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+      ])
+      .pipe(gulp.dest('./vendor/bootstrap'))
 
-  // Font Awesome
-  gulp.src([
-      './node_modules/@fortawesome/**/*',
-    ])
-    .pipe(gulp.dest('./vendor'))
+   // Font Awesome
+   gulp.src([
+         './node_modules/@fortawesome/**/*',
+      ])
+      .pipe(gulp.dest('./vendor'))
 
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
+   // jQuery
+   gulp.src([
+         './node_modules/jquery/dist/*',
+         '!./node_modules/jquery/dist/core.js'
+      ])
+      .pipe(gulp.dest('./vendor/jquery'))
 
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+   // jQuery Easing
+   gulp.src([
+         './node_modules/jquery.easing/*.js'
+      ])
+      .pipe(gulp.dest('./vendor/jquery-easing'))
 
 });
 
 // Compile SCSS
 gulp.task('css:compile', function() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./css'))
+   return gulp.src('./scss/**/*.scss')
+      .pipe(sass.sync({
+         outputStyle: 'expanded'
+      }).on('error', sass.logError))
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions'],
+         cascade: false
+      }))
+      .pipe(header(banner, {
+         pkg: pkg
+      }))
+      .pipe(gulp.dest('./css'))
 });
 
 // Minify CSS
 gulp.task('css:minify', ['css:compile'], function() {
-  return gulp.src([
-      './css/*.css',
-      '!./css/*.min.css'
-    ])
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+   return gulp.src([
+         './css/*.css',
+         '!./css/*.min.css'
+      ])
+      .pipe(cleanCSS())
+      .pipe(rename({
+         suffix: '.min'
+      }))
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
 });
 
 // CSS
@@ -84,19 +84,19 @@ gulp.task('css', ['css:compile', 'css:minify']);
 
 // Minify JavaScript
 gulp.task('js:minify', function() {
-  return gulp.src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browserSync.stream());
+   return gulp.src([
+         './js/*.js',
+         '!./js/*.min.js'
+      ])
+      .pipe(uglify())
+      .pipe(rename({
+         suffix: '.min'
+      }))
+      .pipe(header(banner, {
+         pkg: pkg
+      }))
+      .pipe(gulp.dest('./js'))
+      .pipe(browserSync.stream());
 });
 
 // JS
@@ -107,16 +107,17 @@ gulp.task('default', ['css', 'js', 'vendor']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+   browserSync.init({
+      server: {
+         baseDir: "./",
+         index: 'index.html'
+      }
+   });
 });
 
 // Dev task
 gulp.task('dev', ['css', 'js', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
+   gulp.watch('./scss/*.scss', ['css']);
+   gulp.watch('./js/*.js', ['js']);
+   gulp.watch('./*.html', browserSync.reload);
 });
